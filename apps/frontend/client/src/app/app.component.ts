f352ce29518205741b833a75efda07377b42e2a7
Vladimir Sh <vladirr@gmail.com>
2021-02-20 17:01:05 +0300

    initial commit

---
+++ apps/frontend/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svvs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend-client';
+}
