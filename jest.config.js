b571888a4757778e60011283235554eb65ceacb6
Vladimir Sh <vladirr@gmail.com>
2021-02-24 20:20:47 +0300

    feat(api app): add api app
    * add api app
    * add proxy config
    * add tags
    
    Closes #4

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/frontend/client', '<rootDir>/apps/frontend/admin'],
+  projects: [
+    '<rootDir>/apps/frontend/client',
+    '<rootDir>/apps/frontend/admin',
+    '<rootDir>/apps/backend/api',
+  ],
 };
