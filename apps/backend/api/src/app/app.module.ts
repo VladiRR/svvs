c08badb32d1111842e9c10c39ca74f3003c54cd2
Vladimir Sh <vladirr@gmail.com>
2021-02-25 17:07:09 +0300

    feat(api): add graphql
    add graphql
    
    Closes #6

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { environment } from '../environments/environment'
+import { GraphQLModule } from '@nestjs/graphql'
+import { AppResolver } from './app.resolver'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...environment.connection,
     }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      context: ({req}) => ({req}),
+      playground: true,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppResolver]
 })
 export class AppModule {
 }
