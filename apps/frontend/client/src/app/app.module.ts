7237a3890b94458c7248fd6a9f63a9751098c2b1
Vladimir Sh <vladirr@gmail.com>
2021-03-01 14:48:07 +0300

    feat(scss): add css-rules system ws=>app/lib
    * add styles folder to frontend-client and frontend-admin
    * add
    bootstrap and angular.material themes
    * add common
    
    mixins/variable/function/placeholders
      default files
    * add app
    
    mixins/variable/function/placeholders
      default files
    * add README =>
    libs folder
    
    Closes #10

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [
+    BrowserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
