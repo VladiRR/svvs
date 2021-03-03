0dc0853ab0e9b8fdd43d649e84013eaf84a92fe8
Vladimir Sh <vladirr@gmail.com>
2021-03-03 17:37:04 +0300

    feat(api-auth): add auth module [WiP]
    * change auth.service.ts
    
    #12

--- apps/backend/api/src/app/auth/services/auth.service.ts
+++ apps/backend/api/src/app/auth/services/auth.service.ts
@@ -1,17 +1,49 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { ISignAuthPayload, ISignAuthResponse } from '@svvs/shared/data-access/interfaces'
+import { JwtService } from '@nestjs/jwt'
+import { UserService } from '../../users/services/user.service'
+import { PasswordService } from './password.service'
+import { UserEntity } from '../../users/entities/user.entity'
+import { environment } from '../../../environments/environment'
 
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+    private readonly passwordService: PasswordService,
+  ) {
+  }
 
-  login(signInPayload: ISignAuthPayload):Promise<ISignAuthResponse> {
+  async validateUser(username: string, pass: string): Promise<Omit<UserEntity, 'password'>> {
 
-    return
-    // {
-    //   accessToken: // JWT
-    //   expiresIn: // JWT
-    //   id: // user service
-    // }
+    const user = await this.userService.findOneByUserName(username)
+
+    const isValid = user ? await this.passwordService.compareHash(pass, user.password) : false
+
+    if (isValid) {
+      delete user.password
+
+      return user
+    }
+    return null
+  }
+
+  async login(signInPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
+
+    const user = await this.validateUser(signInPayload.username, signInPayload.password)
+
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+
+    const payload = {username: user.username, userId: user.id}
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+      expiresIn: new Date(environment.jwt.expiresIn).getDate(),
+      id: user.id,
+    }
   }
 }
