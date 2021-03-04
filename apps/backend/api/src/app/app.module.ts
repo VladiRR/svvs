a48f8aea69c0da9fda029a130141fbbabe09fa8f
Vladimir Sh <vladirr@gmail.com>
2021-03-04 17:04:19 +0300

    feat(api): add resolver scalar & typeORM setting [WiP]
    * add resolvers: [resolverMap] => GraphQLModule
    * add entities: [UserEntity] => TypeOrmModule
    * remove providers: [AppResolver] => app.module.ts
    * change app.resolver.ts
    
    #12

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql'
 
 import { AppController } from './app.controller'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { environment } from '../environments/environment'
-import { GraphQLModule } from '@nestjs/graphql'
-import { AppResolver } from './app.resolver'
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module'
+import { resolverMap } from './app.resolver'
+import { UserEntity } from './users/entities/user.entity'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...environment.connection,
+      entities: [UserEntity],
     }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       context: ({req}) => ({req}),
       playground: true,
+      resolvers: [resolverMap],
     }),
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppResolver]
 })
 export class AppModule {
 }
--- apps/backend/api/src/app/app.resolver.ts
+++ apps/backend/api/src/app/app.resolver.ts
@@ -1,5 +1,26 @@
-import { Resolver } from '@nestjs/graphql'
+import GraphQLJSON from 'graphql-type-json'
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
 
-@Resolver('app')
-export class AppResolver {
+export const resolverMap = {
+  Data: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    serialize(value) {
+      return value.getTime() // Value from the client
+    },
+    parseValue(value) {
+      return new Date(value) // Value send to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(parseInt(ast.value, 10)) // ast value is always in string format
+      }
+      return null // Invalid hard-coded value (not an integer)
+    },
+  }),
+  JSON: GraphQLJSON,
 }
+
+
+
