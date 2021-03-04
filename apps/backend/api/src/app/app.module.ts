1b79cecf10977ce05878543aa49dfed9e29a8fd8
Vladimir Sh <vladirr@gmail.com>
2021-03-04 17:45:19 +0300

    feat(api): add UsersModule => app.module.ts [WiP]
    * add UsersModule => section imports (app.module.ts)
    
    #12

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { environment } from '../environments/environment'
 import { AuthModule } from './auth/auth.module'
 import { resolverMap } from './app.resolver'
 import { UserEntity } from './users/entities/user.entity'
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UserEntity } from './users/entities/user.entity'
       playground: true,
       resolvers: [resolverMap],
     }),
+    UsersModule,
     AuthModule,
   ],
   controllers: [AppController],
