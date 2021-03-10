0ac4530a60c227c320ee3505cf6ee7657be4f456
Vladimir Sh <vladirr@gmail.com>
2021-03-10 20:47:25 +0300

    docs(backend-api): add documentation [WiP]
    * add docs comment placeholders
      => frontend-client & frontend-admin
    
    *
    add "src/environments/**/*.ts"
      => "exclude" section
    tsconfig.compodoc.json
      frontend-client & frontend-admin
    
    #16

--- apps/frontend/admin/src/app/app.component.ts
+++ apps/frontend/admin/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core'
 
+/**
+ * Placeholder comment
+ */
 @Component({
   selector: 'svvs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  /**
+   * Title
+   */
   title = 'frontend-admin'
 }
