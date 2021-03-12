cdfdf1832040e2095d773ffd4931aa3537f1eb7c
Vladimir Sh <vladirr@gmail.com>
2021-03-12 16:05:46 +0300

    feat(ws*client): add ssr dependencies [WiP]
    * add ssr dependencies
    
    #20

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {AppComponent} from './app.component'
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
