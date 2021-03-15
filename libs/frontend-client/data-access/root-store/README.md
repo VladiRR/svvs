ef33e7e9931a625dca01774b091ff53f09e9a7c3
Vladimir Sh <vladirr@gmail.com>
2021-03-15 13:01:17 +0300

    feat(root-store): add root-store lib [WiP]
    * add lib frontend-client/data-access/root-store
    * add root-state.interface
    * add router-url-state.interface
    
    #21

---
+++ libs/frontend-client/data-access/root-store/README.md
@@ -0,0 +1,7 @@
+# frontend-client-data-access-root-store
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test frontend-client-data-access-root-store` to execute the unit tests.
---
+++ libs/frontend-client/data-access/root-store/src/lib/interfaces/root-state.interface.ts
@@ -0,0 +1,19 @@
+import {RouterReducerState} from '@ngrx/router-store'
+import {IRouterUrlState} from './router-url-state.interface'
+
+/**
+ * Root state for all application
+ *
+ * @description
+ *
+ * Import only those modules that should always be in the root repository.
+ * By default, only the router state is loaded into the root.
+ *
+ * Functionality modules should be lazy loaded.
+ */
+export interface IRootState {
+  /**
+   * Router state
+   */
+  router: RouterReducerState<IRouterUrlState>
+}
