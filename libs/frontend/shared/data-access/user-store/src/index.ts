8f200db8fbf5f4a742b11bf622a5e8489cabecef
Vladimir Sh <vladirr@gmail.com>
2021-03-17 15:47:40 +0300

    feat(user-store): add user state [WiP]
    * add +state => user-store
    
    #21

--- libs/frontend/shared/data-access/user-store/src/index.ts
+++ libs/frontend/shared/data-access/user-store/src/index.ts
@@ -1,1 +1,6 @@
+export * from './lib/+state/user.actions';
+export * from './lib/+state/user.reducer';
+export * from './lib/+state/user.selectors';
+export * from './lib/+state/user.models';
+export * from './lib/+state/user.facade';
 export * from './lib/user-store.module';
--- libs/frontend/shared/data-access/user-store/src/lib/user-store.module.ts
+++ libs/frontend/shared/data-access/user-store/src/lib/user-store.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromUser from './+state/user.reducer';
+import { UserEffects } from './+state/user.effects';
+import { UserFacade } from './+state/user.facade';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromUser.USER_FEATURE_KEY, fromUser.reducer),
+    EffectsModule.forFeature([UserEffects]),
+  ],
+  providers: [UserFacade],
 })
 export class UserStoreModule {}
