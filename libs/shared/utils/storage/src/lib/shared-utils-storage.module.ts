17ab0bf7758a2e0ee139d0dd4d368c43689b784a
Vladimir Sh <vladirr@gmail.com>
2021-03-24 17:32:32 +0300

    feat(shared-utils-storage): add storage [WiP]
    * change nx.json
      add tags => shared-utils-storage
    
    * change
    shared-utils-storage.module.ts
      add static forRoot
      add all storage =>
      providers
    
    Closes #30

--- libs/shared/utils/storage/src/lib/shared-utils-storage.module.ts
+++ libs/shared/utils/storage/src/lib/shared-utils-storage.module.ts
@@ -1,7 +1,45 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ModuleWithProviders, NgModule} from '@angular/core'
+import {
+  ICookieService,
+  ICookieStorage,
+  ILocalStorage,
+  IMemoryStorage,
+  ISessionStorage,
+  IStorageOptions,
+} from '@svvs/shared/utils/interfaces'
+import {BaseCookieService} from './services/base-cookie.service'
+import {BaseSessionStorage} from './storage/base-session.storage'
+import {BaseLocalStorage} from './storage/base-local.storage'
+import {BaseMemoryStorage} from './storage/base-memory.storage'
+import {BaseCookieStorage} from './storage/base-cookie.storage'
 
-@NgModule({
-  imports: [CommonModule],
-})
-export class SharedUtilsStorageModule {}
+@NgModule()
+export class StorageModule {
+  static forRoot(option: Partial<IStorageOptions> = {}): ModuleWithProviders<StorageModule> {
+    return {
+      ngModule: StorageModule,
+      providers: [
+        {
+          provide: ICookieService,
+          useClass: option.cookieService || BaseCookieService,
+        },
+        {
+          provide: ICookieStorage,
+          useClass: option.cookieStorage || BaseCookieStorage,
+        },
+        {
+          provide: ISessionStorage,
+          useClass: option.sessionStorage || BaseSessionStorage,
+        },
+        {
+          provide: ILocalStorage,
+          useClass: option.localStorage || BaseLocalStorage,
+        },
+        {
+          provide: IMemoryStorage,
+          useClass: option.memoryStorage || BaseMemoryStorage,
+        },
+      ],
+    }
+  }
+}
