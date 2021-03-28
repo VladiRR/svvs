a6d29a8961fc8e141634daa247702cf061a5499b
Vladimir Sh <vladirr@gmail.com>
2021-03-28 19:33:17 +0300

    chore(shared-utils-store): reorient imports [WiP]
    * reorient exports => index.ts
    
    Closes #44

--- libs/shared/utils/store/src/index.ts
+++ libs/shared/utils/store/src/index.ts
@@ -1,3 +1,3 @@
+export * from './lib/effects/abstract-effects'
 export * from './lib/payload-creators/payload-creator'
 export * from './lib/payload-creators/payload-force-creator'
-export * from './lib/effects/abstract-effects'
