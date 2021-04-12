c264ca4283a3de7024b54ac55850777609b2c249
Vladimir Sh <vladirr@gmail.com>
2021-04-12 14:14:18 +0300

    feat(auth-block*auth-store): change auth libs [WiP]
    * add auth-facade.interface.ts
    
    #38

--- libs/frontend/shared/data-access/auth-store/src/lib/interfaces/auth-facade.interface.ts
+++ libs/frontend/shared/data-access/auth-store/src/lib/interfaces/auth-facade.interface.ts
@@ -1,6 +1,56 @@
+import {Observable} from 'rxjs'
+import {ISignAuthPayload} from '@svvs/shared/utils/interfaces'
+import {ApolloError} from '@apollo/client'
 
-
-
+/**
+ * User facade interface
+ */
 export abstract class IAuthFacade {
+  /**
+   * SignIn observer
+   */
+  signIn$: Observable<ISignAuthPayload>
 
+  /**
+   * SignInRun status observer
+   */
+  signInRun$: Observable<boolean>
+
+  /**
+   * SignInError observer
+   */
+  signInError$: Observable<ApolloError>
+
+  /**
+   * SignOut status observer
+   */
+  signOutRun$: Observable<boolean>
+
+  /**
+   * SignOut error observer
+   */
+  signOutError$: Observable<ApolloError>
+
+  /**
+   * Sign in
+   * @param force
+   */
+  abstract signIn(force?: boolean): void
+
+  /**
+   * Set sign in payload
+   * @param payload
+   */
+  abstract signInSet(payload: ISignAuthPayload): void
+
+  /**
+   * Clear sign in payload
+   */
+  abstract signInClear(): void
+
+  /**
+   * Sign out
+   * @param force
+   */
+  abstract signOut(force?: boolean): void
 }
