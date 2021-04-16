c9a7bce92e4be019c9b4a9714eb2a3573ec6819a
Vladimir Sh <vladirr@gmail.com>
2021-04-16 11:21:29 +0300

    refactor(client): move => core.module [WiP]
    * move storage.module => core.module
    * move auth.module => core.module
    
    #38

--- libs/frontend/client/feature/auth/src/lib/auth.module.ts
+++ libs/frontend/client/feature/auth/src/lib/auth.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core'
-import {authContainers, authRoutes} from './auth.common'
+import {authContainers} from './auth.common'
 import {AuthStoreModule} from '@svvs/frontend/shared/data-access/auth-store'
-import {RouterModule} from '@angular/router'
 import {LoginFormModule} from '@svvs/frontend/client/ui/login-form'
 
 @NgModule({
   imports: [
     AuthStoreModule.forRoot(),
     LoginFormModule,
-    RouterModule.forChild(authRoutes),
   ],
   exports: [...authContainers],
   declarations: [...authContainers],
