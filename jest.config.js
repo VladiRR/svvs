554732f32e7981b3962d51949bbf16f617ebfdd9
Vladimir Sh <vladirr@gmail.com>
2021-10-16 22:41:27 +0300

    refactor(WS): update => angular 12[WiP]
    * migrate => nx 12 / angular 12
    
    NB: no change, all service work, all
    autochange diffs warninig not fixed
    
    Todo: clear all warning
    no issues

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,10 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
-    '<rootDir>/apps/frontend/client',
-    '<rootDir>/apps/frontend/admin',
-    '<rootDir>/apps/backend/api',
+    ...getJestProjects(),
     '<rootDir>/libs/shared/data-access/interfaces',
     '<rootDir>/libs/frontend-client/data-access/root-store',
-    '<rootDir>/libs/frontend/shared/data-access/users-store',
-    '<rootDir>/libs/frontend/shared/data-access/auth-store',
-    '<rootDir>/libs/shared/utils/store',
-    '<rootDir>/libs/shared/utils/storage',
-    '<rootDir>/libs/frontend/client/feature/home',
-    '<rootDir>/libs/frontend/client/feature/auth',
-    '<rootDir>/libs/frontend/client/feature/user-profile',
     '<rootDir>/libs/frontend/ui/components',
-    '<rootDir>/libs/frontend/client/ui/user-profile',
-    '<rootDir>/libs/frontend/client/ui/login-form',
   ],
 };
